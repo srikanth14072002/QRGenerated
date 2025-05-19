dd3128c7bbc1997b9e16d81e01c81cdbfd112b59
srikanthraj <137384368+srikanth14072002@users.noreply.github.com>
2025-05-19 08:45:27 +0530

    Add files via upload

---
+++ qrgenerate.js
@@ -0,0 +1,20 @@
+
+let imgBox = document.getElementById("imgBox");
+let qrImage =document.getElementById("qrImage");
+let qrtext =document.getElementById("qrtext");
+function generateQR(){
+    if(qrtext.value.length > 0)
+{
+     qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" +qrtext.valuue; 
+
+    imgBox.classList.add("show-img");
+
+}
+else{
+    qrtext.classList.add('error');
+    setTimeout(()=>{
+          qrtext.classList.remove('error');
+    },1000);
+}
+   
+}
